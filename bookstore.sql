4ee10bbdc4dc9e57a4cd872ed60ce9e982b095df
furukawa2525 <74999996+furukawa2525@users.noreply.github.com>
2021-05-12 14:57:59 +0900

    Create bookstore.sql
    DB作成

---
+++ bookstore.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: 127.0.0.1
+-- 生成日時: 2021-05-12 07:57:09
+-- サーバのバージョン： 10.4.18-MariaDB
+-- PHP のバージョン: 7.4.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `book`
+--
+
+CREATE TABLE `book` (
+  `num` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `book`
+--
+
+INSERT INTO `book` (`num`, `name`, `price`) VALUES
+(1, '星の王子さま', 900);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `favorite`
+--
+
+CREATE TABLE `favorite` (
+  `user_num` int(11) NOT NULL,
+  `book_num` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `purchase`
+--
+
+CREATE TABLE `purchase` (
+  `num` int(11) NOT NULL,
+  `user_num` int(11) NOT NULL,
+  `book_num` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `user`
+--
+
+CREATE TABLE `user` (
+  `num` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `id` varchar(20) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `user`
+--
+
+INSERT INTO `user` (`num`, `name`, `id`, `mail`, `password`) VALUES
+(1, '上村', 'kami', 'kami@gmail.com', 'aaa');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`num`);
+
+--
+-- テーブルのインデックス `favorite`
+--
+ALTER TABLE `favorite`
+  ADD PRIMARY KEY (`user_num`,`book_num`),
+  ADD KEY `book_num` (`book_num`);
+
+--
+-- テーブルのインデックス `purchase`
+--
+ALTER TABLE `purchase`
+  ADD PRIMARY KEY (`num`,`date`),
+  ADD KEY `user_num` (`user_num`),
+  ADD KEY `book_num` (`book_num`);
+
+--
+-- テーブルのインデックス `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`num`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `book`
+--
+ALTER TABLE `book`
+  MODIFY `num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- テーブルの AUTO_INCREMENT `user`
+--
+ALTER TABLE `user`
+  MODIFY `num` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `favorite`
+--
+ALTER TABLE `favorite`
+  ADD CONSTRAINT `favorite_ibfk_1` FOREIGN KEY (`user_num`) REFERENCES `user` (`num`),
+  ADD CONSTRAINT `favorite_ibfk_2` FOREIGN KEY (`book_num`) REFERENCES `book` (`num`);
+
+--
+-- テーブルの制約 `purchase`
+--
+ALTER TABLE `purchase`
+  ADD CONSTRAINT `purchase_ibfk_1` FOREIGN KEY (`user_num`) REFERENCES `user` (`num`),
+  ADD CONSTRAINT `purchase_ibfk_2` FOREIGN KEY (`book_num`) REFERENCES `book` (`num`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
