eca9c2bbe63b6db5693ab3f4c36d853e2ce00933
furukawa2525 <74999996+furukawa2525@users.noreply.github.com>
2021-05-17 20:29:05 +0900

    パスワードのテキストボックスを2つに変更

--- user_input.php
+++ user_input.php
@@ -6,25 +6,25 @@
 <head>
 	<meta charset="UTF-8">
 	<title>会員登録画面</title>
+	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 </head>
 
 <body>
-<i style="color:darkblue;" class="fas fa-crow"></i>
 	<?php require 'menu.php'; ?>
-    <form action="user_output.php" method="post">
+    <form action="db_connect.php" method="post">
 	<table>
-	<tr><th><label for="name">名前</label></th>
+	<tr><th><label for="name" >名前</label></th>
 	<td><input type="text" name="name" value=""></td>
-	</tr>
-	<tr><th><label for="id">ID</label></th>
-	<td><input type="text" name="id" value=""></td>
 	</tr>
 	<tr><th><label for="address">メールアドレス</label></th>
 	<td><input type="text" name="mail" value=""></td>
 	</tr>
 	<tr><th><label for="password">パスワード</label></th>
-	<td><input type="password" name="password" value=""></td>
+	<td><input type="password" name="password" value="" placeholder="6~20文字"></td>
+	</tr>
+	<tr><th><label for="password2">パスワード(確認用)</label></th>
+	<td> <input type="password" name="password2" value=""></td>
 	</tr>
 	<tr><th></th><td><input type="submit" value="登録"></td></tr>
 	</table>
