ee2555b9ff01caadd71bf45e3b93e12c7862671a
furukawa2525 <74999996+furukawa2525@users.noreply.github.com>
2021-05-21 11:35:38 +0900

    update

--- db_connect.php
+++ db_connect.php
@@ -5,7 +5,7 @@
   // 利用するデータベース
   $dbName = 'bookstore';
   // MySQLサーバ
-  $host = 'localhost';
+  $host = 'db-mysql.c09iafkngt3a.us-east-1.rds.amazonaws.com';
   // MySQLのDSN文字列
   $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
   //MySQLデータベースに接続する
